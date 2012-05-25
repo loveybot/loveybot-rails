0e26009ce60229dbc3274474164cd714e21ff362
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-25 13:40:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loveybot::Application.config.secret_token = 'b7c66284a856dec63a4e7ce9a6fa4cb5c7af00f90b2cb5a274f45b0c5131371bf323bfc1bd8825a561f95748ae75f4221b69c58feb7bc68a3dc8d5c9e880e733'
